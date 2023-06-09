c03a13918865f861c79658595bdbd859f1ba74e7
Alessandro Dalbesio <alessandro.dalbesio@epfl.ch>
2023-06-09 10:45:38 +0200

    Reorganized installer and updated README file

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Server
+# API Server
+This repository has been developed as part of the project **"Mixed Reality Environment For Harvesting Study"** done by Alessandro Dalbesio.
+
+
 ## Getting started
 This repository contains all the files needed to implement a server on a Raspberry Pi which can be used to manage all the files and texture needed in a virtual environment. <br><br>
 After the installation as soon as you will turn on the Raspberry it will: 
@@ -14,7 +17,8 @@ A schema of the server is displayed below: <br>
 ![Alt Text](readme/server-schema.png)
 
 ## Installation
-Flash into a microSD the Raspberry Pi OS with Desktop (Raspberry Pi Imager is suggested for this task) and setup the device.<br>
+Flash into a microSD (use at least 16 GB) the Raspberry Pi OS with Desktop 32/64 bit (Raspberry Pi Imager is suggested for this task) and setup the device.<br>
+You should be able also to use other OS but you need to be sure that they are Debian based (or the installation script will not work).<br>
 Don't use the WiFi to connect the device to the internet but use the Ethernet cable.
 ### Set up the Raspberry Hotspot
 To create an hotspot with Raspberry Pi please follow [this tutorial](https://www.tomshardware.com/how-to/raspberry-pi-access-point).<br>
@@ -23,27 +27,49 @@ The name you will choose for the network and its password is not important for t
 ### Installation
 To install the server you should follow these steps:
 1. Open a new terminal
-2. Update and upgrade the system
+2. Create a new directory
 ```bash
-apt-get upgrade
-apt-get update -y
+sudo mkdir /home/<username>/server
 ```
-3. Create a folder
-```bash
-mkdir /usr/local/server
-```
+The default username is <b>pi</b>.
 3. Clone the repository
 ```bash
-git clone https://gitlab.epfl.ch/create-lab/sensing-with-vr/server.git /usr/local/server
+git clone https://gitlab.epfl.ch/create-lab/sensing-with-vr/api-server.git /home/<username>/server
 ```
-4. Modify the parameters in <code>modules/settings.py</code> and <code>install.sh</code> to make the server compatible with your needs. A brief description of the parameters is provided in the files.
+It will ask you to insert your credentials. You should use the username and the access token generated to access <code>gitlab.epfl.ch</code> (the access token will be needed also in the next sections).<br>
+4. After cloning the repository move to the folder
+```bash
+cd /home/<username>/server
+```
+5. Create a file called <code>parameters.env</code> and write inside it the following parameters:
+```bash
+website_repo_url="create-lab/sensing-with-vr/user-interface"
+websocket_server_repo_url="create-lab/sensing-with-vr/websocket-server"
+api_server_repo_url="create-lab/sensing-with-vr/api-server"
+api_port=9000
+websocket_port=9001
+domain_name="virtualenv.epfl.ch"
+username="..."
+access_token="..."
+```
+Above the default parameters are displayed. You should modify them to make the server compatible with your needs. A brief description of the parameters is provided below:
+- <code>website_repo_url</code>: The repository of the user interface. If you want to use the default user interface you should write <code>create-lab/sensing-with-vr/user-interface</code>. If you want to use a custom user interface you should write the repository of the user interface you want to use.
+- <code>websocket_server_repo_url</code>: The repository of the websocket server. If you want to use the default websocket server you should write <code>create-lab/sensing-with-vr/websocket-server</code>. If you want to use a custom websocket server you should write the repository of the websocket server you want to use.
+- <code>api_server_repo_url</code>: The repository of the API server. You should not modify this parameter.
+- <code>api_port</code>: The port used by the API server.
+- <code>websocket_port</code>: The port used by the websocket server.
+- <code>domain_name</code>: The domain name that you want to use
+- <code>username</code>: The username of the account used to access <code>gitlab.epfl.ch</code> (it is your email without the @epfl.ch)
+- <code>access_token</code>: The access token generated to access <code>gitlab.epfl.ch</code>
+
+Take into account that all the <code>.env</code> files are ignored so that you don't push your credentials by mistake on the repository.<br>
+At the end of the installation script the folder will be deleted so you don't need to worry about leaving your credentials on the Raspberry. If you wish after completing the installation you can delete the access token from your account.
 5. Run the installation scripts
 ```bash
-sudo bash /usr/local/server/install.sh
+sudo bash ./install.sh
 ```
-6. Go to /var/www and modify the <code>settings</code> files of the services
-7. Restart the system
-8. When the system has started up check that no errors are present in the log files
+This script will install all the needed components and it will take a while. <br>
+If the installation has been succesful if you try to access the domain name you should see the user interface.<br>
 
 ## Code Structure
 The code structure is:
@@ -81,13 +107,13 @@ The Websocket Server choosen doesn't affect the API server but affect the abilit
 
 ## Errors management
 All the errors produced by the server will be writen in files available in the folder <code>/var/www/API</code>. In this folder there will be both the file for the errors generated from the server and the file for the errors generated by <code>supervisord</code> which is a process control system used to manage the server <br>
-If something goes wrong the server will generate automatically an unique ID that can be used to identify the error.
+If something goes wrong the server will generate automatically an unique ID (that will be shown on the user interface) that can be used to identify the error.
 
 ## Development conditions
 The system has been developed in the following conditions:
 - Python 3.11
-- Raspberry Pi OS 32-bit with desktop kernel version 5.15 and Debian version 11
-- Raspberry Pi 3 B+
+- Raspberry Pi OS 32-bit / 64-bit with desktop kernel version 5.15 and Debian version 11
+- Raspberry Pi 3 Model B+ (even if it should work also with the higher models)
 
 
 ## Authors
--- install.sh
+++ install.sh
@@ -1,35 +1,38 @@
-# !/bin/bash
-
+#!/bin/bash
 # This file has been created specifically for Debian Distributions
 # Some changes may be needed for other distributions
 
-## CHANGE BELOW THIS LINE ##
-#---------------------------#
-
-website_repo_url=""
-websocket_server_repo_url=""
-api_server_repo_url=""
-
-#-----------------------------------------#
-## CHANGE ABOVE THIS LINE ##
+# To define the parameters copy the following in a file called "parameters.env" and change the values
+# Then run this script with the command "sudo bash install.sh"
+##### COPY BELOW THIS LINE #####
+# Insert the gitlab repository urls (after the https://gitlab.epfl.ch/ part)
+# website_repo_url="create-lab/sensing-with-vr/user-interface"
+# websocket_server_repo_url="create-lab/sensing-with-vr/websocket-server"
+# api_server_repo_url="create-lab/sensing-with-vr/api-server"
+# api_port=9000
+# websocket_port=9001
+# domain_name="virtualenv.epfl.ch"
+# Insert your git credentials for gitlab.epfl.ch
+# username="" # Without the @epfl.ch
+# access_token=""
+##### COPY ABOVE THIS LINE #####
 
 # Step 0: Definition of the parameters and initial checking
-    # Get script location
-    script_path=$(dirname $(readlink -f $0))
+    # Load the parameters from parameters.env
+    if [ -f parameters.env ]; then
+        source parameters.env
+    fi
 
-    # Get nginx files location
-    nginx_conf_file="$script_path/setup-files/nginx/nginx.conf"
-    nginx_proxy_file="$script_path/setup-files/nginx/virtual_env"
+    # Check if the parameters have been defined
+    if [ -z "$website_repo_url" ] || [ -z "$websocket_server_repo_url" ] || [ -z "$api_server_repo_url" ] || [ -z "$api_port" ] || [ -z "$websocket_port" ] || [ -z "$domain_name" ] || [ -z "$username" ] || [ -z "$access_token" ]; then
+        echo "Please define the parameters in the file parameters.sh"
+        exit 1
+    fi
 
-    # Get supervisor files location
-    supervisor_api_file="$script_path/setup-files/supervisor/api.conf"
-    supervisor_websocket_file="$script_path/setup-files/supervisor/websocket.conf"
-
-    # Get dnsmaq files location
-    dnsmasq_conf_file="$script_path/setup-files/dnsmasq/dnsmasq.conf"
-
-    # Get the dhcpd.conf file location
-    dhcpd_conf_file="$script_path/setup-files/dhcpd/dhcpd.conf"
+    # Add the gitlab credentials to the urls
+    website_repo_url="https://$username:$access_token@gitlab.epfl.ch/$website_repo_url"
+    websocket_server_repo_url="https://$username:$access_token@gitlab.epfl.ch/$websocket_server_repo_url"
+    api_server_repo_url="https://$username:$access_token@gitlab.epfl.ch/$api_server_repo_url"
 
     # Verify that the system is Debian
     if [ ! -f /etc/debian_version ]; then
@@ -37,67 +40,271 @@ api_server_repo_url=""
         exit 1
     fi
 
-    # Manage python
-    if [ -f /usr/bin/python ]; then
-        python=$(which python)
-    elif [ -f /usr/bin/python3 ]; then
-        python=$(which python3)
-    else
-        sudo apt-get install python3 -y
-        python=$(which python3)
+    # Verify that the system has git installed and install it if not
+    if [ ! -f /usr/bin/git ]; then
+        sudo apt-get install git -y
     fi
 
+    # Get the current device ip on wlan0
+    ip=$(ip addr show wlan0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
+    # Apply to the ip the mask 24
+    router_ip=$(echo $ip | cut -d. -f1-3).0
+
 # Step 1: Set the static IP
-    sudo cp $dhcpd_conf_file /etc/dhcpd.conf
+    dhcpcd_conf_file="
+    # Inform the DHCP server of our hostname for DDNS.
+    hostname
+
+    # Use the hardware address of the interface for the Client ID.
+    clientid
+
+    # Persist interface configuration when dhcpcd exits.
+    persistent
+
+    # Rapid commit support.
+    # Safe to enable by default because it requires the equivalent option set
+    # on the server to actually work.
+    option rapid_commit
+
+    # A list of options to request from the DHCP server.
+    option domain_name_servers, domain_name, domain_search, host_name
+    option classless_static_routes
+    # Respect the network MTU. This is applied to DHCP routes.
+    option interface_mtu
+
+    # A ServerID is required by RFC2131.
+    require dhcp_server_identifier
+
+    # Generate Stable Private IPv6 Addresses based from the DUID
+    slaac private
+
+    # Static IP configuration for WLAN
+    interface wlan0
+    static ip_address=$ip/24
+    static routers=$router_ip
+    static domain_name_server=$router_ip"
+
+    sudo rm /etc/dhcpcd.conf
+    sudo touch /etc/dhcpcd.conf
+    # Save the file into the other file without outputting it
+    echo "$dhcpcd_conf_file" | sudo tee /etc/dhcpcd.conf > /dev/null
 
 # Step 2: Update and upgrade
     sudo apt-get update
     sudo apt-get upgrade -y
+    sudo apt autoremove -y
 
 # Step 3: Install Nginx
     sudo apt-get install nginx -y
 
 # Step 4: Configure Nginx
-    sudo cp $nginx_conf_file /etc/nginx/nginx.conf
-    sudo cp $nginx_proxy_file /etc/nginx/sites-available/virtual_env
+    nginx_conf_file='
+    user www-data;
+    worker_processes auto;
+    pid /run/nginx.pid;
+    include /etc/nginx/modules-enabled/*.conf;
+
+    events {
+        worker_connections 2000;
+        # multi_accept on;
+    }
+
+    http {
+
+        ##
+        # Basic Settings
+        ##
+
+        sendfile on;
+        tcp_nopush on;
+        types_hash_max_size 2048;
+        # server_tokens off;
+
+        server_names_hash_bucket_size 64;
+        # server_name_in_redirect off;
+
+        include /etc/nginx/mime.types;
+        default_type application/octet-stream;
+
+        ##
+        # SSL Settings
+        ##
+
+        ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3; # Dropping SSLv3, ref: POODLE
+        ssl_prefer_server_ciphers on;
+
+        ##
+        # Logging Settings
+        ##
+
+        access_log /var/log/nginx/access.log;
+        error_log /var/log/nginx/error.log;
+
+        ##
+        # Gzip Settings
+        ##
+
+        gzip on;
+
+        # gzip_vary on;
+        # gzip_proxied any;
+        # gzip_comp_level 6;
+        # gzip_buffers 16 8k;
+        # gzip_http_version 1.1;
+        # gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+
+        ##
+        # Virtual Host Configs
+        ##
+
+        include /etc/nginx/conf.d/*.conf;
+        include /etc/nginx/sites-enabled/*;
+        client_max_body_size 100M;
+    }'
+    nginx_proxy_file='
+    server {
+        listen 80;
+        server_name '$domain_name';
+        error_log /var/www/server.error.log;
+        
+        location / {
+            root /var/www/website;
+            index index.html;
+        }
+
+        location /ws {
+            proxy_pass http://127.0.0.1:'$websocket_port'/;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "Upgrade";
+            proxy_set_header Host $host;
+        }
+
+        location /api {
+            proxy_pass http://127.0.0.1:'$api_port'/;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_pass_request_headers on;
+        }
+
+        location /models {
+            alias /var/www/models/;
+            autoindex off;
+        }
+    }'
+
+    # Copy the nginx configuration file
+    sudo rm /etc/nginx/nginx.conf
+    sudo touch /etc/nginx/nginx.conf
+    # Copu the file while keeping the new lines
+    
+    echo "$nginx_conf_file" | sudo tee -a /etc/nginx/nginx.conf > /dev/null
+
+    # Copy the nginx proxy file
+    sudo rm /etc/nginx/sites-available/*
+    sudo rm /etc/nginx/sites-enabled/*
+    sudo touch /etc/nginx/sites-available/virtual_env
+    echo "$nginx_proxy_file" | sudo tee -a /etc/nginx/sites-available/virtual_env > /dev/null
     sudo ln -s /etc/nginx/sites-available/virtual_env /etc/nginx/sites-enabled/virtual_env
-    sudo rm /etc/nginx/sites-enabled/default
 
 # Step 5: Create the directories and clone the repositories
+    # Remove all the material that is in the folder /var/www
+    sudo rm -rf /var/www/*
+
     # Manage website
     sudo mkdir -p /var/www/website
     sudo git clone $website_repo_url /var/www/website
+    website_settings='{
+    "SELF_DOMAIN": "http://'$domain_name'/",
+    "API_DOMAIN": "http://'$domain_name'/api",
+    "MODELS_FOLDER": "http://'$domain_name'/models/",
+    "WEBSOCKET_DOMAIN": "ws://'$domain_name'/ws"
+    }'
+    echo $website_settings | sudo tee -a /var/www/website/settings.json > /dev/null
 
     # Manage websocket server
     sudo mkdir -p /var/www/websocket
-    sudo git clone $websocket_server_repo_url /var/www/websocket
-    cd /var/www/website
-    sudo $python -m venv venv
-    source venv/bin/activate
-    sudo pip install -r requirements.txt
-    deactivate
+    sudo git clone ${websocket_server_repo_url} /var/www/websocket
+    sudo python -m venv /var/www/websocket/.venv
+    sudo /var/www/websocket/.venv/bin/pip install -r /var/www/websocket/requirements.txt
+    websocket_server_settings='{
+        "WEBSOCKET_SERVER_ADDRESS": "127.0.0.1",
+        "WEBSOCKET_SERVER_PORT": '$websocket_port'
+    }'
+    echo $websocket_server_settings | sudo tee -a /var/www/websocket/modules/settings.json > /dev/null
 
     # Manage API server
     sudo mkdir -p /var/www/API
     sudo git clone $api_server_repo_url /var/www/API
-    cd /var/www/API
-    sudo $python -m venv venv
-    source venv/bin/activate
-    sudo pip install -r requirements.txt
-    deactivate
+    sudo python -m venv /var/www/API/.venv
+    sudo /var/www/API/.venv/bin/pip install -r /var/www/API/requirements.txt
 
 # Step 6: Install supervisor
     sudo apt-get install supervisor -y
 
 # Step 7: Configure supervisor
-    sudo cp $supervisor_api_file /etc/supervisor/conf.d/api.conf
-    sudo cp $supervisor_websocket_file /etc/supervisor/conf.d/websocket.conf
+    supervisor_api_file="
+    [program:api]
+    directory=/var/www/API
+    command=/var/www/API/.venv/bin/gunicorn index:app -b localhost:$api_port
+    autostart=true
+    autorestart=true
+    stderr_logfile=/var/www/API/supervisor.err.log
+    stdout_logfile=/var/www/API/supervisor.out.log"
+
+    supervisor_websocket_file="
+    [program:websocket]
+    command=/var/www/websocket/.venv/bin/python /var/www/websocket/index.py
+    autostart=true
+    autorestart=true
+    stderr_logfile=/var/www/websocket/supervisor.err.log
+    stdout_logfile=/var/www/websocket/supervisor.out.log"
+
+    # Remove all the files in the folder /etc/supervisor/conf.d
+    sudo rm -rf /etc/supervisor/conf.d/*
+    # Create the first file called api.conf
+    sudo touch /etc/supervisor/conf.d/api.conf
+    echo "$supervisor_api_file" | sudo tee -a /etc/supervisor/conf.d/api.conf > /dev/null
+    # Create the second file called websocket.conf
+    sudo touch /etc/supervisor/conf.d/websocket.conf
+    echo "$supervisor_websocket_file" | sudo tee -a /etc/supervisor/conf.d/websocket.conf > /dev/null
+    sudo supervisorctl reread
+    sudo supervisorctl update
 
 # Step 8: Install dnsmasq
     sudo apt-get install dnsmasq -y
 
 # Step 9: Configure dnsmasq
-    sudo cp $dnsmasq_conf_file /etc/dnsmasq.conf
+    # Stop and disable systemd-resolved
+    sudo systemctl disable systemd-resolved
+    sudo systemctl stop systemd-resolved
+
+    # Define dnsmasq configuration file
+    dnsmasq_conf_file="
+    # Configuration file for dnsmasq.
+
+    # Never forward plain names (without a dot or domain part)
+    domain-needed
+
+    # Never forward addresses in the non-routed address spaces.
+    bogus-priv
+
+    # Define the virtualenv address
+    address=/$domain_name/$ip
+    
+    # Set the DNS servers
+    server=8.8.8.8
+    server=8.8.4.4"
+
+    # Delete the file /etc/dnsmasq.conf
+    sudo rm /etc/dnsmasq.conf
+    sudo touch /etc/dnsmasq.conf
+    echo "$dnsmasq_conf_file" | sudo tee -a /etc/dnsmasq.conf > /dev/null
 
 # Step 10: Final configurations
-    sudo rm -rf $script_path
+    #sudo rm $(pwd)/parameters.env
+    #sudo rm $(pwd)/install.sh
+    sudo reboot
